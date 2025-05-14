d029ee81826d516cedea149b2fd78cb7b699eb04
belen2008 <belencastaneta19@gmail.com>
2025-05-14 11:10:22 +0000

    belen

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-variable=19
-variable="belen" 
-variable=true#beelean 
-variable=None
-Print(variable)#None
+variable1=19
+variable2="belen" 
+variable3=True #belen
+variable4=None
+comparacion=variable=19
+print(comparacion)
+print(variable)#None
+
+variable=1 
+comparacion=variable==1 
+print(comparacion)
+comparacion=not comparacion
+print(comparacion)
+mayor=variable>0
+print(mayor)
+menor=variable<=0
+print(menor)
+
+mayor=(1+1)>1
+menor=(20-30)<10
+y=mayor and menor 
+print(y)
